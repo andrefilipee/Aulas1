bedd60b9c57d8bd7023878c4ebc141174859d884
andrefilipee <andrefilipe.rodrigues@my.istec.pt>
2022-10-18 23:01:20 +0100

    teste2

--- Exercicio1/Program.cs
+++ Exercicio1/Program.cs
@@ -7,6 +7,11 @@ namespace Exercicio1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Hello World!");
+
+
+
         }
     }
 }
